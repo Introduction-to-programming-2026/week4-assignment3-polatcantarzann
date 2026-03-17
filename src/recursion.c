4828a4c26c55637304fb2267a89b1fe0c9a2e54e
polatcantarzann <polat.can.tarzan@ibu.edu.mk>
2026-03-17 09:53:37 +0000

    finished recursion

--- src/recursion.c
+++ src/recursion.c
@@ -18,3 +18,41 @@ Rules:
 - You cannot use a loop to print rows.
 - Base case must be correct.
 */
+
+#include <stdio.h>
+
+void draw(int n);
+
+int main(void)
+{
+    int height;
+    // Ask for positive integer
+    do
+    {
+        printf("Height: ");
+        scanf("%d", &height);
+    }
+    while (height < 1);
+
+    // Call recursive function
+    draw(height);
+}
+
+void draw(int n)
+{
+    // Base case: If height is 0, stop
+    if (n <= 0)
+    {
+        return;
+    }
+
+    // Draw a pyramid of height n - 1 (The recursive call)
+    draw(n - 1);
+
+    // Print the n-th row
+    for (int j = 0; j < n; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
