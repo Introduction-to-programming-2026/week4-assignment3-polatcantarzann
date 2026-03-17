24cb526dfd6e8ab52efc8a8af50438547bed81ae
polatcantarzann <polat.can.tarzan@ibu.edu.mk>
2026-03-17 09:51:47 +0000

    finished phonebook

--- src/phonebook.c
+++ src/phonebook.c
@@ -20,3 +20,49 @@ Rules:
 - You must use struct.
 - Use strcmp() to compare names.
 */
+
+#include <stdio.h>
+#include <string.h>
+
+// 1. Create a struct Person
+typedef struct
+{
+    char *name;
+    char *phone;
+}
+person;
+
+int main(void)
+{
+    // 2. Create an array of persons (Size changed to 3 to include you)
+    person people[3];
+
+    people[0].name = "Carter";
+    people[0].phone = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].phone = "+1-949-468-2750";
+
+    // Adding you to the list
+    people[2].name = "polat";
+    people[2].phone = "+26262626";
+
+    // 3. Ask the user for a name
+    char name[50];
+    printf("Name: ");
+    scanf("%s", name);
+
+    // 4. Search for the name (Looping through 3 people now)
+    for (int i = 0; i < 3; i++)
+    {
+        if (strcmp(people[i].name, name) == 0)
+        {
+            printf("Found %s\n", people[i].phone);
+            return 0;
+        }
+    }
+
+    // 5. If not found
+    printf("Not found\n");
+    return 1;
+}
